77412ae5d8fb109082901ae76b4b5d27e8464db1
William C <2899523@gmail.com>
2026-09-07 21:40:14 +0000

    bug: remove debug code

--- qualtran/drawing/musical_score_test.py
+++ qualtran/drawing/musical_score_test.py
@@ -47,7 +47,6 @@ def test_musical_score_aligns_with_qubit_count():
         # Ensure qubits (horizontal rows) match qubits from cost values in expected count. This
         # test may depend on how elements of a circuit are represented.
         actual_qubits = msd.max_y + 1
-        assert False, f'{"\n".join(f"{x.symb}: {x.rpos.topo_gen}" for x in msd.soqs)}'
         assert (
             msd.max_y + 1 == expected_qubits
         ), f'{type(bloq).__name__} has too many lines - expected {expected_qubits}; got {msd.max_y + 1}'
